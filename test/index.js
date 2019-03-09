3173fd61c77a9020a0b64e01b85a9dc2c4a62314
Will Binns-Smith <wbinnssmith@gmail.com>
2019-03-08 19:34:12 -0800

    Add instanceof check

--- test/index.js
+++ test/index.js
@@ -6,4 +6,6 @@ const A2 = require('@wbinnssmith/test/A');
 let a1: A1 = new A1();
 let a2: A2 = a1;
 
-console.log(A1 === A2)
+console.log(A1 === A2);
+console.log(new A2() instanceof A1);
+console.log(new A1() instanceof A2);
